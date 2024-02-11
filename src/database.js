581c29d91384f5cbdd554cb1c1c133697d8b8492
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2024-02-11 02:25:35 -0300

    atualiza o nome da URI do banco de dados

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
 dotenv.config()
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URI)
 
 const FilmSchema = new mongoose.Schema({
   id: Number,
