c5509e9bd13b8f0a670920e4f5ea71c77ad65d6f
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2024-02-11 02:24:05 -0300

    redireciona o arquivo src/routes.js para src/routes/films.routes.js

--- src/routes/films.routes.js
+++ src/routes/films.routes.js
@@ -1,26 +1,26 @@
 import express from 'express'
 
-import { Film } from './database.js'
+import { Film } from '../database.js'
 
-export const router = express.Router()
+export const filmRouter = express.Router()
 
-router.get('/', async (req, res) => {
+filmRouter.get('/', async (req, res) => {
   res.send('Hello World!')
 })
 
-router.get('/movie', async (req, res) => {
+filmRouter.get('/movie', async (req, res) => {
   const movie = await Film.find()
 
   res.json(movie)
 })
 
-router.get('/movie/:id', async (req, res) => {
+filmRouter.get('/movie/:id', async (req, res) => {
   const movie = await Film.findById(req.params.id)
 
   res.json(movie)
 })
 
-router.post('/movie', async (req, res) => {
+filmRouter.post('/movie', async (req, res) => {
   console.log(req.body)
   const movie = new Film({
     id: Number(req.body.id),
@@ -33,14 +33,14 @@ router.post('/movie', async (req, res) => {
   res.json(movie)
 })
 
-router.put('/movie/:id', async (req, res) => {
+filmRouter.put('/movie/:id', async (req, res) => {
   const movie = await Film.findByIdAndUpdate(
     req.params.id,
     {
       title: req.body.title,
       director: req.body.director,
       releaseYear: req.body.releaseYear,
-      poster: req.body.poster,
+      // poster: req.body.poster,
     },
     { new: true },
   )
@@ -48,7 +48,7 @@ router.put('/movie/:id', async (req, res) => {
   res.json(movie)
 })
 
-router.delete('/movie/:id', async (req, res) => {
+filmRouter.delete('/movie/:id', async (req, res) => {
   const movie = await Film.findByIdAndDelete(req.params.id)
 
   res.json(movie)
